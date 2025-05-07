42af12ef866f408ee53ba7cb2505c7cb9176388d
Gerjan <gerjan.nienhuis@gmail.com>
2025-05-07 14:21:35 +0200

    added practice page

---
+++ resources/views/game/practice.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="bg-[url('../../public/img/background4.png')] bg-cover bg-center h-screen">
+    <div class="flex flex-col justify-center items-center h-screen gap-4 p-7">
+        <h1 class="text-center text-4xl font-chakra p-10">Oefenvraag</h1>
+        <p class="text-center text-2xl">Je krijgt zo een voorbeeld vraag te zien</p>
+        <p class="text-center text-2xl">Dit is ook hoe later de andere vragen zullen zijn</p>
+        <a href="#" class="bg-[#3289BB] border-black border border-solid rounded-full p-2 w-2/3 text-center"><p>Verder -></p></a>
+    </div>
+</div>
+
+@endsection
