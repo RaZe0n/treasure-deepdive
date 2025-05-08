3b2070685295a16f0899e7905694a5b1f3470338
Gerjan <gerjan.nienhuis@gmail.com>
2025-05-08 12:31:57 +0200

    hint page gemaakt

---
+++ resources/views/game/hint.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="bg-[url('../../public/img/background5.png')] bg-cover bg-center h-screen">
+    <div class="flex flex-col justify-center items-center h-screen gap-4">
+        <h1 class="text-center text-5xl font-chakra p-10">Hint</h1>
+        <p class="text-center text-3xl p-10">Bij het standbeeld zit een bordje met informatie</p>
+    </div>
+</div>
+
+@endsection
